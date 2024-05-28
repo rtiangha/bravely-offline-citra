b1c3478af93a0123b7306b88ba99eb6862878fce
OpenSauce04 <opensauce04@gmail.com>
2024-05-28 12:42:25 +0100

    Android: Fixed landscape layouts not displaying correctly

--- src/video_core/renderer_opengl/renderer_opengl.cpp
+++ src/video_core/renderer_opengl/renderer_opengl.cpp
@@ -700,6 +700,7 @@ void RendererOpenGL::DrawScreens(const Layout::FramebufferLayout& layout, bool f
 }
 
 void RendererOpenGL::ApplySecondLayerOpacity() {
+#ifndef ANDROID // TODO: Implement custom layouts on Android
     if ((Settings::values.layout_option.GetValue() == Settings::LayoutOption::CustomLayout ||
         Settings::values.custom_layout) &&
         Settings::values.custom_second_layer_opacity.GetValue() < 100) {
@@ -709,9 +710,11 @@ void RendererOpenGL::ApplySecondLayerOpacity() {
         state.blend.dst_rgb_func = GL_ONE_MINUS_CONSTANT_ALPHA;
         state.blend.color.alpha = Settings::values.custom_second_layer_opacity.GetValue() / 100.0f;
     }
+#endif
 }
 
 void RendererOpenGL::ResetSecondLayerOpacity() {
+#ifndef ANDROID // TODO: Implement custom layouts on Android
     if ((Settings::values.layout_option.GetValue() == Settings::LayoutOption::CustomLayout ||
         Settings::values.custom_layout) &&
         Settings::values.custom_second_layer_opacity.GetValue() < 100) {
@@ -721,6 +724,7 @@ void RendererOpenGL::ResetSecondLayerOpacity() {
         state.blend.dst_a_func = GL_ZERO;
         state.blend.color.alpha = 0.0f;
     }
+#endif
 }
 
 void RendererOpenGL::DrawTopScreen(const Layout::FramebufferLayout& layout,
