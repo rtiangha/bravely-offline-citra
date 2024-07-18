a11dece0ccf529444894c286cebf642284b25bb0
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-07-18 13:41:28 -0600

    Add x16 frame skip option

--- src/android/app/src/main/jni/default_ini.h
+++ src/android/app/src/main/jni/default_ini.h
@@ -101,7 +101,7 @@ use_cpu_jit =
 cpu_clock_percentage =
 
 # The applied frameskip amount. Must be a power of two.
-# 0 (default): No frameskip, 1: x2 frameskip, 2: x4 frameskip, 3: x8 frameskip, etc.
+# 0 (default): No frameskip, 1: x2 frameskip, 2: x4 frameskip, 3: x8 frameskip, x16 frameskip.
 frame_skip =
 
 [Renderer]
