88c7c380724e6c3ea98177a9d1d5990a88a71c71
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-27 23:10:30 -0600

    Personal: Set `-Ofast` CFLAGS and target `skylake` on msys2 & `armv8.2-a` on Android

--- .ci/android.sh
+++ .ci/android.sh
@@ -10,7 +10,6 @@ fi
 # Build Vulkan-ValidationLayers
 mkdir -p src/android/app/build/tmp
 mkdir -p assets/libs/lib/arm64-v8a
-mkdir -p assets/libs/lib/x86_64
 
 cd externals/Vulkan-ValidationLayers
 
@@ -19,13 +18,13 @@ cmake -S . -B build \
   -D CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
   -D CMAKE_C_COMPILER_LAUNCHER=ccache \
   -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -D CMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang++ \
-  -D CMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang \
-  -D CMAKE_CXX_FLAGS=-O2 \
-  -D CMAKE_C_FLAGS=-O2 \
+  -D CMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang++ \
+  -D CMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android34-clang \
+  -D CMAKE_CXX_FLAGS="-Ofast -march=armv8.2-a" \
+  -D CMAKE_C_FLAGS="-Ofast -march=armv8.2-a" \
   -D CMAKE_EXE_LINKER_FLAGS=-flto=thin \
   -D CMAKE_SHARED_LINKER_FLAGS=-flto=thin \
-  -D ANDROID_PLATFORM=28 \
+  -D ANDROID_PLATFORM=34 \
   -D CMAKE_ANDROID_ARCH_ABI=arm64-v8a \
   -D CMAKE_ANDROID_STL_TYPE=c++_static \
   -D ANDROID_USE_LEGACY_TOOLCHAIN_FILE=NO \
@@ -36,31 +35,6 @@ cmake -S . -B build \
 cmake --build build
 cmake --install build --prefix build/libs/arm64-v8a
 mv build/libs/arm64-v8a/lib/libVkLayer_khronos_validation.so ../../assets/libs/lib/arm64-v8a
-
-rm -rf build
-
-# Build vvl release binary for x86_64
-cmake -S . -B build \
-  -D CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
-  -D CMAKE_C_COMPILER_LAUNCHER=ccache \
-  -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -D CMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android28-clang++ \
-  -D CMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android28-clang \
-  -D CMAKE_CXX_FLAGS=-O2 \
-  -D CMAKE_C_FLAGS=-O2 \
-  -D CMAKE_EXE_LINKER_FLAGS=-flto=thin \
-  -D CMAKE_SHARED_LINKER_FLAGS=-flto=thin \
-  -D ANDROID_PLATFORM=28 \
-  -D CMAKE_ANDROID_ARCH_ABI=x86_64 \
-  -D CMAKE_ANDROID_STL_TYPE=c++_static \
-  -D ANDROID_USE_LEGACY_TOOLCHAIN_FILE=NO \
-  -D CMAKE_BUILD_TYPE=Release \
-  -D UPDATE_DEPS=ON \
-  -G Ninja
-
-cmake --build build
-cmake --install build --prefix build/libs/x86_64
-mv build/libs/x86_64/lib/libVkLayer_khronos_validation.so ../../assets/libs/lib/x86_64
 
 cd ../../assets/libs
 zip -r Vulkan-ValidationLayers.zip lib
