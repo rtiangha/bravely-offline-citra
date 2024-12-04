163fa8aaf33b4786b12e68ff688784ec1279ae03
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-05 00:33:42 +0530

    SearchLocationHelper: Implement saving and retrieving logic

--- src/android/app/src/main/java/io/github/borked3ds/android/utils/SearchLocationHelper.kt
+++ src/android/app/src/main/java/io/github/borked3ds/android/utils/SearchLocationHelper.kt
@@ -4,6 +4,8 @@ package io.github.borked3ds.utils
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import io.github.borked3ds.android.Borked3DSApplication
+import io.github.borked3ds.android.utils.GameHelper
 
 interface SearchLocationHelper {
     companion object {
@@ -13,7 +15,8 @@ interface SearchLocationHelper {
          * @return A list of URIs of selected search locations
          */
         fun getSearchLocations(context: Context): List<Uri> {
-            val locations = "" // TODO: Implement locations obtaining logic
+            val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
+            val locations = preferences.getString(GameHelper.KEY_GAME_PATH, "")
             val urisList = mutableListOf<Uri>()
 
             locations.forEach {
@@ -31,11 +34,11 @@ interface SearchLocationHelper {
          */
         fun addLocation(context : Context, uri : Uri) : SearchLocationResult {
             var locations = getSearchLocations(context)
-            if(locations.contains(uri)){
+            val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
+            if (locations.contains(uri)) {
                 return SearchLocationResult.AlreadyAdded
-            }
-            else{
-                // TODO: Implement saving logic
+            } else {
+                preferences.edit().putString(GameHelper.KEY_GAME_PATH, StringBuilder().append(getSearchLocations(context)).append(if(locations.isEmpty()) "" else "|").append(uri.toString()).toString()).apply()
             }
             return SearchLocationResult.Success
         }
@@ -46,13 +49,14 @@ interface SearchLocationHelper {
          * @return The exit status of the installation process
          */
         fun deleteLocation(context : Context, uri : Uri) : SearchLocationResult {
-            var locations = ""
-
+            var locations = getSearchLocations(context)
+            val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
             var newValue = ""
             for (location in locations) {
                 if(location != uri.toString() && location != "")
                     newValue = StringBuilder().append(newValue).append(if(newValue.isEmpty()) "" else "|").append(location).toString()
             }
+            preferences.edit().putString(GameHelper.KEY_GAME_PATH, newValue).apply()
             return SearchLocationResult.Deleted
         }
     }
