81c866583957028cf6e5514bcf86a5ea9f7df057
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-08 11:54:45 +0530

    Fix

--- src/android/app/src/main/java/io/github/borked3ds/android/utils/SearchLocationHelper.kt
+++ src/android/app/src/main/java/io/github/borked3ds/android/utils/SearchLocationHelper.kt
@@ -4,6 +4,7 @@ package io.github.borked3ds.android.utils
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import androidx.preference.PreferenceManager
 import io.github.borked3ds.android.Borked3DSApplication
 import io.github.borked3ds.android.utils.GameHelper
 
@@ -16,7 +17,7 @@ interface SearchLocationHelper {
          */
         fun getSearchLocations(context: Context): List<Uri> {
             val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
-            val locations = preferences.getString(GameHelper.KEY_GAME_PATH, "").split("|")
+            val locations = preferences.getString(GameHelper.KEY_GAME_PATH, "").orEmpty().split("|")
             val urisList = mutableListOf<Uri>()
 
             locations.forEach {
@@ -29,34 +30,45 @@ interface SearchLocationHelper {
 
         /**
          * Adds the given search location to the emulation settings
-         * @param uri The uri os the selected search location
+         * @param uri The URI of the selected search location
          * @return The exit status of the installation process
          */
-        fun addLocation(context : Context, uri : Uri) : SearchLocationResult {
-            var locations = getSearchLocations(context)
+        fun addLocation(context: Context, uri: Uri): SearchLocationResult {
+            val locations = getSearchLocations(context)
             val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
+
             if (locations.contains(uri)) {
                 return SearchLocationResult.AlreadyAdded
             } else {
-                preferences.edit().putString(GameHelper.KEY_GAME_PATH, StringBuilder().append(getSearchLocations(context)).append(if(locations.isEmpty()) "" else "|").append(uri.toString()).toString()).apply()
+                val newLocations = buildString {
+                    append(locations.joinToString(separator = "|") { it.toString() })
+                    if (locations.isNotEmpty()) append("|")
+                    append(uri.toString())
+                }
+
+                preferences.edit()
+                    .putString(GameHelper.KEY_GAME_PATH, newLocations)
+                    .apply()
             }
             return SearchLocationResult.Success
         }
 
         /**
-         * Deletes the given uri from the emulation settings
-         * @param uri The input stream to read the driver from
-         * @return The exit status of the installation process
+         * Deletes the given URI from the emulation settings
+         * @param uri The URI to remove from the list
+         * @return The exit status of the removal process
          */
-        fun deleteLocation(context : Context, uri : Uri) : SearchLocationResult {
-            var locations = getSearchLocations(context)
+        fun deleteLocation(context: Context, uri: Uri): SearchLocationResult {
+            val locations = getSearchLocations(context)
             val preferences = PreferenceManager.getDefaultSharedPreferences(Borked3DSApplication.appContext)
-            var newValue = ""
-            for (location in locations) {
-                if(location != uri.toString() && location != "")
-                    newValue = StringBuilder().append(newValue).append(if(newValue.isEmpty()) "" else "|").append(location).toString()
-            }
-            preferences.edit().putString(GameHelper.KEY_GAME_PATH, newValue).apply()
+
+            val newValue = locations.filterNot { it.toString() == uri.toString() }
+                .joinToString(separator = "|") { it.toString() }
+
+            preferences.edit()
+                .putString(GameHelper.KEY_GAME_PATH, newValue)
+                .apply()
+
             return SearchLocationResult.Deleted
         }
     }
